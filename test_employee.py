055143f217ea017da3b6d17b5490bc59891f5ff1
samuelm-LCWC <samuelm@lcwc.ac.uk>
2024-12-02 10:21:49 +0000

    v1.0

--- test_employee.py
+++ test_employee.py
@@ -1,1 +1,7 @@
+from employee import Employee
+import pytest
 
+def test_employee():
+    test_case = Employee("John", "Smith")
+    assert test_case.fullname == "John Smith"
+    assert test_case.email == "john.smith@company.com"
